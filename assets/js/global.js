41ac82abe44b2384ee47a69f98bdb4f0d01eddce
GongHanna <han_nah2853@naver.com>
2024-06-03 18:00:12 +0900

    240603 css 수정 animation 추가 js 이벤트 추가

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,138 @@
+// a 요소가 가지고 있는 기본 이벤트 제거
+$('a[href="#"]').on('click', (e) => {
+  e.preventDefault();
+});
+
+// 로고 클릭 시 페이지 맨 위로 이동
+$('.header .logo').on('click', (e) => {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
 // 헤더 햄버거 메뉴 버튼 클릭 시 이벤트 발생
 $('.header .side-menu-wrapper .side-menu-btn').on('click', () => {
   $('.header .side-menu-wrapper .side-menu-btn').toggleClass('active');
   $('.header .side-menu-wrapper .side-menu').slideToggle().toggleClass('show');
 });
+
+// 헤더 변경 스크롤트리거
+gsap.registerPlugin(ScrollTrigger);
+const triggers = document.querySelectorAll('.common-trigger');
+
+triggers.forEach(trigger => {
+  if (!trigger.classList.contains('no-trigger')) {
+    ScrollTrigger.create({
+      trigger: trigger,
+      start: 'top top',
+      end: 'bottom top',
+      toggleActions: 'play reverse play reverse',
+      onEnter: () => {
+        gsap.to(".side-menu-btn", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+        gsap.to(".side-menu", {
+          backgroundColor: '#fff',
+          color: 'var(--primary-black)',
+          duration: 0.4
+        });
+        gsap.to(".logo", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+        gsap.to(".header .code-desc", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+      },
+      onLeave: () => {
+        gsap.to(".side-menu-btn", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+        gsap.to(".side-menu", {
+          backgroundColor: 'var(--primary-black)',
+          color: '#fff',
+          duration: 0.4
+        });
+        gsap.to(".logo", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+        gsap.to(".header .code-desc", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+      },
+      onEnterBack: () => {
+        gsap.to(".side-menu-btn", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+        gsap.to(".side-menu", {
+          backgroundColor: '#fff',
+          color: 'var(--primary-black)',
+          duration: 0.4
+        });
+        gsap.to(".logo", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+        gsap.to(".header .code-desc", {
+          filter: 'invert(100%)',
+          duration: 0.4
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(".side-menu-btn", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+        gsap.to(".side-menu", {
+          backgroundColor: 'var(--primary-black)',
+          color: '#fff',
+          duration: 0.4
+        });
+        gsap.to(".logo", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+        gsap.to(".header .code-desc", {
+          filter: 'invert(0)',
+          duration: 0.4
+        });
+      }
+    });
+  }
+});
+
+const headerInvert = {
+  trigger: '.common-trigger',
+  start: 'top top',
+  end: 'bottom top',
+  toggleActions: 'play reverse play reverse',
+  onEnter: () =>
+    gsap.to(".global-menu-button", {
+      filter: 'invert(100%)',
+      duration: 0.4
+    }),
+  onLeave: () =>
+    gsap.to(".global-menu-button", {
+      filter: 'invert(0)',
+      duration: 0.4
+    }),
+  onEnterBack: () =>
+    gsap.to(".global-menu-button", {
+      filter: 'invert(100%)',
+      duration: 0.4
+    }),
+  onLeaveBack: () =>
+    gsap.to(".global-menu-button", {
+      filter: 'invert(0)',
+      duration: 0.4
+    }),
+};
+
+ScrollTrigger.create(headerInvert);
--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-// a 요소가 가지고 있는 기본 이벤트 제거
-$('a[href="#"]').on('click', (e) => {
-  e.preventDefault();
-});
+// 해당 컨텐츠로 부드러운 이동
+$('.header .side-menu-wrapper .side-menu li a').each(function(idx, elem) {
+  $(elem).on('click', (e) => {
+    e.preventDefault();
 
-// 로고 클릭 시 페이지 맨 위로 이동
-$('.header .logo').on('click', (e) => {
-  scrollTo({
-    top: 0,
-    behavior: 'smooth'
+    const contentsHt = $('section').eq(idx).offset().top;
+    scrollTo({
+      top: contentsHt,
+      behavior: 'smooth'
+    });
   });
 });
-
-// 마우스 따라 움직이는 눈
-// $(document).mousemove(function (event) {
-//   $('.eye').each(function () {
-//     const eye = $(this);
-//     const centerX = eye.offset().left + eye.width() / 2;
-//     const centerY = eye.offset().top + eye.height() / 2;
-
-//     const angle = Math.atan2(event.pageY - centerY, event.pageX - centerX);
-//     const rotateDeg = angle * (180 / Math.PI) + 90;
-
-//     eye.css('transform', `rotate(${rotateDeg}deg)`);
-//   });
-// });
 
 // 랜덤 텍스트 이벤트
 $(function () {
@@ -61,109 +47,6 @@ $('.skill-contents .skill-wrapper li').on('click', (e) => {
   $(e.currentTarget).toggleClass('show').siblings().removeClass('show');
 });
 
-// 헤더 변경 스크롤트리거
-gsap.registerPlugin(ScrollTrigger);
-const triggers = document.querySelectorAll('.common-trigger');
-
-triggers.forEach(trigger => {
-  if (!trigger.classList.contains('no-trigger')) {
-    ScrollTrigger.create({
-      trigger: trigger,
-      start: 'top top',
-      end: 'bottom top',
-      toggleActions: 'play reverse play reverse',
-      onEnter: () => {
-        gsap.to(".side-menu-btn", {
-          filter: 'invert(100%)',
-          duration: 0.4
-        });
-        gsap.to(".side-menu", {
-          backgroundColor: '#fff',
-          color: 'var(--primary-black)',
-          duration: 0.4
-        });
-        gsap.to(".logo", {
-          filter: 'invert(100%)',
-          duration: 0.4
-        });
-      },
-      onLeave: () => {
-        gsap.to(".side-menu-btn", {
-          filter: 'invert(0)',
-          duration: 0.4
-        });
-        gsap.to(".side-menu", {
-          backgroundColor: 'var(--primary-black)',
-          color: '#fff',
-          duration: 0.4
-        });
-        gsap.to(".logo", {
-          filter: 'invert(0)',
-          duration: 0.4
-        });
-      },
-      onEnterBack: () => {
-        gsap.to(".side-menu-btn", {
-          filter: 'invert(100%)',
-          duration: 0.4
-        });
-        gsap.to(".side-menu", {
-          backgroundColor: '#fff',
-          color: 'var(--primary-black)',
-          duration: 0.4
-        });
-        gsap.to(".logo", {
-          filter: 'invert(100%)',
-          duration: 0.4
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to(".side-menu-btn", {
-          filter: 'invert(0)',
-          duration: 0.4
-        });
-        gsap.to(".side-menu", {
-          backgroundColor: 'var(--primary-black)',
-          color: '#fff',
-          duration: 0.4
-        });
-        gsap.to(".logo", {
-          filter: 'invert(0)',
-          duration: 0.4
-        });
-      }
-    });
-  }
-});
-
-const headerInvert = {
-  trigger: '.common-trigger',
-  start: 'top top',
-  end: 'bottom top',
-  toggleActions: 'play reverse play reverse',
-  onEnter: () =>
-    gsap.to(".global-menu-button", {
-      filter: 'invert(100%)',
-      duration: 0.4
-    }),
-  onLeave: () =>
-    gsap.to(".global-menu-button", {
-      filter: 'invert(0)',
-      duration: 0.4
-    }),
-  onEnterBack: () =>
-    gsap.to(".global-menu-button", {
-      filter: 'invert(100%)',
-      duration: 0.4
-    }),
-  onLeaveBack: () =>
-    gsap.to(".global-menu-button", {
-      filter: 'invert(0)',
-      duration: 0.4
-    }),
-};
-
-ScrollTrigger.create(headerInvert);
 
 // .rolled-over-txt
 gsap.utils.toArray('.rolled-over-txt').forEach((txt) => {
@@ -171,7 +54,7 @@ gsap.utils.toArray('.rolled-over-txt').forEach((txt) => {
     scrollTrigger: {
       trigger: txt,
       start: '100% 100%',
-      end: '100% 0',
+      end: '50% 50%',
       scrub: 1
     }
   }).fromTo(
